1b15857caa688467c5fa3ac38acc79126eda8a6c
Seth Bowman <brandonsethbowman@gmail.com>
2023-05-16 11:47:13 -0500

    Exercise completed

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            GuessingGame();
+        }
+
+        public static void GuessingGame()
+        {
+            var r = new Random();
+            var favNum = r.Next(1, 10);
+            var response = 0;
+
+            while (response != favNum)
+            {
+                Console.WriteLine("Give me a number");
+                response = int.Parse(Console.ReadLine());
+
+                if (response < favNum)
+                {
+                    Console.WriteLine($"Too low!\nYour guess: {response}");
+                }
+                else if (response > favNum)
+                {
+                    Console.WriteLine($"Too high\nYour guess: {response}");
+                }
+                else
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+
+            }
         }
     }
 }
